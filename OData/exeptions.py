0fe90b0f05f4ae09823b3a8f4bbc413e77ec3a91
Aleksey Kravtsun <kravtsun.aleksey@gmail.com>
2024-10-02 13:30:06 +0300

    Переписал Connection с использованием requests.Session

---
+++ OData/exeptions.py
@@ -0,0 +1,8 @@
+class ODataError(Exception):
+    pass
+
+class ResponseError(ODataError):
+    def __init__(self, status, reason, details):
+        super().__init__(
+            f'Status: {status}. Reason: {reason}. Details: {details}')
+
--- OData/sample.py
+++ OData/sample.py
@@ -1,5 +1,20 @@
 from OData.odata import OData, Q
+from OData.connection import Connection, auth
 
+authentication = auth.HTTPBasicAuth('pro2', 'dev')
+
+conn = Connection('http://erp.polipak.local/', 'erp_dev', authentication)
+conn.session.close()
+
+
+data = conn.list('Document_ЭтапПроизводства2_2',
+                 {'$top': 2, '$select': 'Number,Ref_Key'})
+print(data)
+
+data = conn.get('Document_ЭтапПроизводства2_2',
+                'f75e2f51-6c60-11ec-aa38-ac1f6bd30991',
+                {'$select': 'Number, Ref_Key'})
+print(data)
 from pydantic import BaseModel, Field
 
 # class FooSerializer(BaseModel):
@@ -12,15 +27,15 @@ from pydantic import BaseModel, Field
 #     width: int = Field(validation_alias='Ширина')
 #     length: int = Field(validation_alias='Длина')
 
-class FooSerializer(BaseModel):
-    a: int = Field()
-    b: int = Field(validation_alias='Фуу')
-    c: int = Field(validation_alias='Bar')
-    d: int = Field()
-
-class FooOdata(OData):
-    serializer_class = FooSerializer
-    pass
+# class FooSerializer(BaseModel):
+#     a: int = Field()
+#     b: int = Field(validation_alias='Фуу')
+#     c: int = Field(validation_alias='Bar')
+#     d: int = Field()
+#
+# class FooOdata(OData):
+#     serializer_class = FooSerializer
+#     pass
 
 
 
@@ -28,8 +43,8 @@ class FooOdata(OData):
 # q = (Q(a=10) | Q(c=10)) & ~Q(d=20)
 # q = (~Q(d=20) | ~Q(c='abc')) & Q(b__eq__guid='123-321')
 # q = Q(Q(a=10))
-q = ~Q(c=10) & ~Q(d=20)
-
-
-odata = FooOdata().filter(q)
-print(odata.build_filter())
+# q = ~Q(c=10) & ~Q(d=20)
+#
+#
+# odata = FooOdata().filter(q)
+# print(odata.build_filter())
